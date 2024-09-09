95de9c8525958159c2af6c1f4eb056cdd8984a9d
introvert730180 <107503675+introvert730180@users.noreply.github.com>
2024-09-09 18:34:13 +0530

    final project

--- config/mongoose-connection.js
+++ config/mongoose-connection.js
@@ -1,11 +1,14 @@
 const mongoose =require('mongoose');
-
+const dbr =require("debug")("development:mongoose");
 mongoose
-.connect("mongobd://127.0.0.1:27017/shop")
+.connect("mongodb://127.0.0.1:27017/shop")
 .then(function(){
+    dbr("connected");
     console.log("connected");
 })
 .catch(function(err){
+    dbr(err);
     console.log(err);
-})
+
+});
 module.exports=mongoose.connection;
--- models/product-model.js
+++ models/product-model.js
@@ -1,16 +1,16 @@
-const mongoose =require('mongoose');
+const mongoose = require('mongoose') ;
 
-const productSchema = mongoose.Schema({
-    image : String,
-    name: String,
-    price : Number,
-    dicount :{
-        tupe : Number,
-        default: 0
-    },
-    bgcolor : String,
-    panelcolor : String,
-    textcolor : String,
-});
+const ProductSchema = mongoose.Schema({
+     name: String,
+     price:Number,
+     discount:{
+        type:Number,
+        default:0 
+     },
+     image:Buffer, 
+     bgcolor:String, 
+     panelcolor:String, 
+     textcolor:String, 
+})
 
-module.exports= mongoose.model('"product', productSchema);
+module.exports = mongoose.model("productSchema" , ProductSchema)
